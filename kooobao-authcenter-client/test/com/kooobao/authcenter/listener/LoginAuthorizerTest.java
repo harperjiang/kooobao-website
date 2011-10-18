a2f614d85caa09553f6cb78806d235e21b8f61d2
harperjiang@gmail.com <harperjiang@gmail.com@0fce3ec8-7071-de33-013e-020e0f3b7730>
2011-10-18 10:22:25 +0000

    Authenticate

---
+++ kooobao-authcenter-client/test/com/kooobao/authcenter/listener/LoginAuthorizerTest.java
@@ -0,0 +1,14 @@
+package com.kooobao.authcenter.listener;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginAuthorizerTest {
+
+	@Test
+	public void testNeedValidate() {
+		fail("Not yet implemented");
+	}
+
+}
