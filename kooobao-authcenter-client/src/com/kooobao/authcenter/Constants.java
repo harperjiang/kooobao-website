133738eb50e96d1ef5627da12069d5277992d06c
harperjiang@gmail.com <harperjiang@gmail.com@0fce3ec8-7071-de33-013e-020e0f3b7730>
2011-10-18 10:54:29 +0000

    Authenticate:JumpBack

---
+++ kooobao-authcenter-client/src/com/kooobao/authcenter/Constants.java
@@ -0,0 +1,8 @@
+package com.kooobao.authcenter;
+
+public interface Constants {
+
+	public static final String JUMP_URL = "com.kooobao.authcenter.JumpUrl";
+	
+	public static final String TOKEN = "com.kooobao.authcenter.Token";
+}
--- kooobao-authcenter-client/src/com/kooobao/authcenter/web/bean/LoginBean.java
+++ kooobao-authcenter-client/src/com/kooobao/authcenter/web/bean/LoginBean.java
@@ -1,12 +1,14 @@
 package com.kooobao.authcenter.web.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.kooobao.authcenter.listener.LoginAuthorizer;
+import com.kooobao.authcenter.Constants;
 import com.kooobao.authcenter.service.AuthenticateService;
 import com.kooobao.authcenter.service.Token;
 import com.kooobao.common.web.bean.AbstractBean;
@@ -15,18 +17,46 @@ import com.kooobao.common.web.bean.AbstractBean;
 @SessionScoped
 public class LoginBean extends AbstractBean {
 
+
 	private String userId;
 
 	private String plainPass;
-	
+
 	@ManagedProperty("#{authenticateService}")
 	private AuthenticateService authService;
 
 	public String login() {
-		HttpSession httpSession = (HttpSession) FacesContext
-				.getCurrentInstance().getExternalContext().getSession(false);
-		httpSession.setAttribute(LoginAuthorizer.TOKEN, new Token());
+		Token token = validateLogin();
+		if(token == null) {
+			addMessage(FacesMessage.SEVERITY_ERROR,"登录失败");
+			return "false";
+		}
+		putTokenInSession(token);
+		jumpUrl();
+
 		return "true";
+	}
+
+	private void jumpUrl() {
+		javax.servlet.http.HttpServletResponse response = (HttpServletResponse) javax.faces.context.FacesContext
+				.getCurrentInstance().getExternalContext().getResponse();
+		Object previousUrl = getSession().getAttribute(Constants.JUMP_URL);
+		// Jump to previous URL
+		if(null!=previousUrl)
+		response.setHeader("Location",String.valueOf(previousUrl));
+	}
+	
+	protected static HttpSession getSession() {
+		return (HttpSession) FacesContext
+				.getCurrentInstance().getExternalContext().getSession(false);
+	}
+
+	private void putTokenInSession(Token token) {
+		getSession().setAttribute(Constants.TOKEN, token);
+	}
+
+	private Token validateLogin() {
+		return null;
 	}
 
 	public String getUserId() {
@@ -53,5 +83,4 @@ public class LoginBean extends AbstractBean {
 		this.authService = authService;
 	}
 
-	
 }
